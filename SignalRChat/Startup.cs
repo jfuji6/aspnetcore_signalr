715c636df227a3e2dda1955bf4422d94f651982a
jfuji6 <fuji1@cox.net>
2018-05-20 15:06:14 -0700

    Simplify

--- SignalRChat/Startup.cs
+++ SignalRChat/Startup.cs
@@ -14,33 +14,11 @@ namespace SignalRChat
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSignalR();
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            
-            //services.AddCors(options => options.AddPolicy("CorsPolicy", 
-            //builder => 
-            //{
-            //    builder.AllowAnyMethod().AllowAnyHeader()
-            //           .WithOrigins("http://localhost:55830")
-            //           .AllowCredentials();
-            //}));
-
             services.AddSignalR();
         }
 
@@ -51,16 +29,8 @@ namespace SignalRChat
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
-
-            // app.UseHttpsRedirection();
+            
             app.UseStaticFiles();
-            app.UseCookiePolicy();
-            //app.UseCors("CorsPolicy");
             app.UseSignalR(routes => 
             {
                 routes.MapHub<ChatHub>("/chathub");
